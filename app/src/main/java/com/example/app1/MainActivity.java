857e5dbb8ee99bf0dd4569de2b0a45b50db3c665
adamdkohler@gmail.com <Babysounder3!>
2020-10-18 12:44:32 -0700

    class assignment Recyclerview working, listing 20 "people", 10 offline 10 online

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,33 +1,35 @@
 package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactAdapter adapter = new ContactAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
-    public void LoginSubmit(View view) {
-        Log.i("Info", "Clicked Submit Button");
-        EditText un = findViewById(R.id.username);
-        String UserNameEntered = un.getText().toString();
-        Log.i( "INFO", "UserName entered is " + UserNameEntered);
-        EditText pw = findViewById(R.id.password);
-        String PassWordEntered = pw.getText().toString();
-        Log.i("INFO", "Password entered is " + PassWordEntered);
 
-        Intent UserConfirmationActivityIntent = new Intent(getApplicationContext(), UserConfirmationActivity.class);
-        UserConfirmationActivityIntent.putExtra("username", UserNameEntered);
-        UserConfirmationActivityIntent.putExtra("password", PassWordEntered);
-        startActivity(UserConfirmationActivityIntent);
-    }
+
 }
